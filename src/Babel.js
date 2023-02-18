eb8666ee11b1f4450e5081b2122874cdfbd00e05
gushenyu1998 <71572428+gushenyu1998@users.noreply.github.com>
2023-02-18 11:56:57 -0800

    aaa

--- src/Babel.js
+++ src/Babel.js
@@ -5,7 +5,7 @@ class EntryPage extends Component {
     constructor(props){
         super(props)
         this.state = {
-            currentView: "signUp"
+            currentView: "Entry"
         }
     }
 
@@ -17,7 +17,7 @@ class EntryPage extends Component {
 
     currentView = () => {
         switch(this.state.currentView) {
-            case "signUp":
+            case "Entry":
                 return (
                     <form>
                         <h2>Lost in Translation</h2>
@@ -35,27 +35,6 @@ class EntryPage extends Component {
                             </ul>
                         </fieldset>
                         <button>Start Chatting!</button>
-                    </form>
-                )
-                break
-            case "PWReset":
-                return (
-                    <form>
-                        <h2>Reset Password</h2>
-                        <fieldset>
-                            <legend>Password Reset</legend>
-                            <ul>
-                                <li>
-                                    <em>A reset link will be sent to your inbox!</em>
-                                </li>
-                                <li>
-                                    <label for="email">Email:</label>
-                                    <input type="email" id="email" required/>
-                                </li>
-                            </ul>
-                        </fieldset>
-                        <button>Send Reset Link</button>
-                        <button type="button" onClick={ () => this.changeView("logIn")}>Go Back</button>
                     </form>
                 )
             default:
