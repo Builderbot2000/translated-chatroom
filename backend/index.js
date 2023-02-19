c10e2df31f59044d1d3203c206b36c916305b798
jaskaran27177 <jaskarandhanoa7@gmail.com>
2023-02-18 20:11:36 -0800

    added front and back end changes

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,18 @@
 require('dotenv').config()
+const mongoose=require('mongoose');
+mongoose.connect(process.env.MONGO_URL,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+    
+}).then(()=>{
+    console.log("DB Connection Sucessful")
+}).catch((err)=>{
+    console.log(err.message)
+});
 const express = require('express')
 const cors = require('cors')
 const Message = require('./models/message')
-const { request, response } = require('express')
+const { request, response, json } = require('express')
 
 const app = express()
 
@@ -25,23 +35,43 @@ app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
-app.get('/getAll', (request, response) => {
+app.get('/getAll', async(request, response) => {
   // Call DB using the "Message" endpoint defined above
   // retrieve all messages from DB 
   // call Google Translate API on every message retrieved to get translated messages
   // send all translated messages as response
   // 
   // YOUR CODE HERE
-  response.json(notes)
+  try {
+    Message.find({}, function(err, messages) {
+           response.json({messages: messages});
+        });
+  } catch (ex) {
+    next(ex);
+  }
 })
 
-app.get('/addMessage', (request, response) => {
+app.post('/addMessage', async(request, response) => {
   // Call DB using the "Message" endpoint defined above
   // save the new message onto DB
   // call the socketIO server
   // send the new message to the socketIO server
   // return confirmation of success or failure
   // YOUR CODE HERE
+  var today = new Date();
+  var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  try{    
+    const user =await Message.create({
+      name: request.body.name,
+      language:request.body.language ,
+      message: request.body.message,
+      time: time
+    });
+    return response.json({status:true})
+}
+catch(ex){
+    next(ex)
+}
 })
 
 const PORT = 3001
