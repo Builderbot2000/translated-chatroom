edbcc28ba75c29f20e1673dad3fb0dc98b673218
gushenyu1998 <71572428+gushenyu1998@users.noreply.github.com>
2023-02-19 10:34:06 -0800

    Complete stylish and s

--- frontend/src/components/ReceiveMessage.js
+++ frontend/src/components/ReceiveMessage.js
@@ -1,9 +1,7 @@
 import '../css/chatroom.scss'
 import Message from './Message';
-import { useState, useEffect, useRef } from 'react';
+import {useEffect, useRef, useState} from 'react';
 import axios from "axios";
-import io from 'socket.io-client';
-import {wait} from "@testing-library/user-event/dist/utils";
 
 
 const ReceiveMessage = ({ username, url, language }) => {
@@ -11,26 +9,28 @@ const ReceiveMessage = ({ username, url, language }) => {
     const messagesColumnRef = useRef(null);
 
     function sortMessagesByDate(messages) {
-        console.log('sort')
-        console.log(messages)
+        const [hours1, minutes1, seconds1] = messages[0].time.split(':');
         return messages.sort(
-            (a, b) => parseInt(a.time) - parseInt(b.time)
-        );
+            (a, b) => {
+                const [hours1, minutes1, seconds1] = a.time.split(':');
+                const [hours2, minutes2, seconds2] = b.time.split(':');
+                const aTime = 60*60*parseInt(hours1) + 60*parseInt(minutes1) + parseInt(seconds1)
+                const bTime = 60*60*parseInt(hours2) + 60*parseInt(minutes2) + parseInt(seconds2)
+                return aTime - bTime
+            }
+        )
     }
     //receive messages
     useEffect(
         // eslint-disable-next-line react-hooks/exhaustive-deps
         () => {
-            const timer = setTimeout(()=>{
+            const Timmer = setTimeout(()=>{
                 axios.get(url.concat('/getAll/fr')).then((r) => {
-                    console.log(r.data[0].time)
                     setMessagesReceived(sortMessagesByDate(r.data))
-
                 })
-                console.log(Date.now())
-            },100)
-            return ()=> clearTimeout(timer)
-        },[url]
+            }, 1000)
+            return ()=> clearTimeout(Timmer)
+        }
     )
 
     //scroll to the most recent message
