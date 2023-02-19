47242e4432d03986ff6860aad06e7aaa8d3ac1f0
Builderbot2000 <kevintang1118@outlook.com>
2023-02-18 18:19:20 -0800

    skeleton

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,14 @@
+require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
+const Message = require('./models/message')
+const { request, response } = require('express')
+
 const app = express()
+
+app.use(express.static('build'))
+app.use(cors())
+app.use(express.json())
 
 let notes = [
   {
@@ -17,7 +26,22 @@ app.get('/', (request, response) => {
 })
 
 app.get('/getAll', (request, response) => {
+  // Call DB using the "Message" endpoint defined above
+  // retrieve all messages from DB 
+  // call Google Translate API on every message retrieved to get translated messages
+  // send all translated messages as response
+  // 
+  // YOUR CODE HERE
   response.json(notes)
+})
+
+app.get('/addMessage', (request, response) => {
+  // Call DB using the "Message" endpoint defined above
+  // save the new message onto DB
+  // call the socketIO server
+  // send the new message to the socketIO server
+  // return confirmation of success or failure
+  // YOUR CODE HERE
 })
 
 const PORT = 3001
